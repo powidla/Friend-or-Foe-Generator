d666719a5e3d8c05c5dd8b69e84f64d98dec9c24
powidla <109307554+powidla@users.noreply.github.com>
2026-05-21 09:44:23 +0200

    change template for processor

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,6 +1,8 @@
+import os
 import numpy as np
-import re
-import json 
+import pandas as pd
+from scipy.io import loadmat
+from sklearn.model_selection import train_test_split
 
-class Preprocessor:
+class Processor:
   ...
